6b2566d8db1f17b62cf378c5af6b4db544ddd31b
Deiverson Mourão Alves Pedroso <deiversson.pedroso@estudante.ufjf.br>
2025-09-23 21:36:17 -0300

    :tada:  Begin a project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Disciplina from './Disciplina'
+function App() {
+  return <>
+    <h1>Salut, monde!</h1>
+    <Disciplina />
+  </>
+}
+
+export default App
---
+++ src/Disciplina.tsx
@@ -0,0 +1,10 @@
+function Disciplina() {
+    return (
+        <div>
+            <div className="disciplina">DCC207</div>
+            <div>Desenvolvimento Web Front-End</div>
+        </div>
+    );
+}
+
+export default Disciplina;
