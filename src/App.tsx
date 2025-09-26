1f656fe19392fdd4bc7399da272ed1d3d910ba0c
Deiverson Mourão Alves Pedroso <deiversson.pedroso@estudante.ufjf.br>
2025-09-25 21:58:31 -0300

    :label: adiciona tipos para a disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Disciplina from './Disciplina'
 function App() {
   return (
     <>
-      <h1>Salut, monde!</h1>
-      <h2>Salut react!</h2>
+      <div className='title'>
+        <h1>Salut, monde!</h1>
+        <h2>Salut react!</h2>
+      </div>
       <Disciplina codigo='DCC202' nome='Desenvolvimento Web I' />
       <Disciplina codigo='DCC206' nome='Desenvolvimento Web II' />
       <Disciplina codigo='DCC207' nome='Desenvolvimento Web Front-End' />
